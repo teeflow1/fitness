fe3aecdffc641554de1fe7011917979607fd2506
“Temitope <temtopeayobami@gmail.com>
2023-08-09 14:57:23 +0100

    Working on frontend

--- gym/settings.py
+++ gym/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'gymapp', #new app
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'gym.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['gym/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
